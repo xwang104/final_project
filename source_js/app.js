daafeff03f4a3139f52f8a79984e8bcb8357ffe9
Xiaoxiao Wang <xiaoxiaowang@Xiaoxiaos-MacBook-Pro.local>
2016-04-20 10:31:37 -0500

    install ui-router; course layout

--- source_js/app.js
+++ source_js/app.js
@@ -1,27 +1,60 @@
-var app = angular.module('courseHelper', ['ngRoute', 'CHControllers', 'CHServices', 'CHDirectives']);
+var app = angular.module('courseHelper', ['ui.router','ngRoute', 'CHControllers', 'CHServices', 'CHDirectives']);
 
-app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-    when('/dues', {
-    templateUrl: 'partials/dues.html',
-    controller: 'DuesController'
-  }).
-  when('/todos', {
-    templateUrl: 'partials/todos.html',
-    controller: 'TodosController'
-  }).
-  when('/stats', {
-    templateUrl: 'partials/stats.html',
-    controller: 'StatsController'
-  }).
-  when('/adddrop', {
-    templateUrl: 'partials/adddrop.html',
-    controller: 'AddDropController'
-  }).
-  otherwise({
-    redirectTo: '/dues'
-  });
+app.config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
+  $urlRouterProvider.otherwise("/dues");
+
+  $stateProvider
+    .state('dues', {
+      url: "/dues",
+        views: {
+            'header': {
+                templateUrl: 'partials/studentheader.html'
+            },
+            'content': {
+                templateUrl: 'partials/dues.html'
+            }
+        },
+      controller: 'DuesController'
+    })
+    .state('todos', {
+      url: "/todos",
+        views: {
+            'header': {
+                templateUrl: 'partials/studentheader.html'
+            },
+            'content': {
+                templateUrl: 'partials/todos.html'
+            }
+        },
+      controller: 'TodosController'
+    })
+    .state('adddrop', {
+      url: "/adddrop",
+        views: {
+            'header': {
+                templateUrl: 'partials/studentheader.html'
+            },
+            'content': {
+                templateUrl: 'partials/adddrop.html'
+            }
+        },
+      controller: 'AddDropController'
+    })
+    .state('course', {
+      url: "/course",
+        views: {
+            'header': {
+                templateUrl: 'partials/courseheader.html'
+            },
+            'content': {
+                templateUrl: 'partials/course.html'
+            }
+        },
+      controller: 'TodosController'
+    });
 }]);
+
+
 
 app.run(function($rootScope) {
     $rootScope.$on('$viewContentLoaded', function () {
